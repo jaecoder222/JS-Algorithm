b19b8d478d128977076bca7e72e3e8243e85fdb4
jaecoder222 <82952382+jaecoder222@users.noreply.github.com>
2022-11-23 15:16:58 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,38 +1,9 @@
 function solution(age) {
     var answer = '';
     var stringAge = String(age);
-    for(var v =0; v < stringAge.length; v++){
-      if(stringAge[v] === '0'){
-          answer = answer + 'a'
-      }
-        if(stringAge[v] === '1'){
-          answer = answer + 'b'
-      }
-        if(stringAge[v] === '2'){
-          answer = answer + 'c'
-      }
-        if(stringAge[v] === '3'){
-          answer = answer + 'd'
-      }
-        if(stringAge[v] === '4'){
-          answer = answer + 'e'
-      }
-        if(stringAge[v] === '5'){
-          answer = answer + 'f'
-      }
-        if(stringAge[v] === '6'){
-          answer = answer + 'g'
-      }
-        if(stringAge[v] === '7'){
-          answer = answer + 'h'
-      }
-        if(stringAge[v] === '8'){
-          answer = answer + 'i'
-      }
-        if(stringAge[v] === '9'){
-          answer = answer + 'j'
-      }
-        
+    var alphabet = ['a','b','c','d','e','f','g','h','i','j']
+    for(var i =0; i < stringAge.length; i++){
+        answer = answer + alphabet[stringAge[i]]
     }
     return answer;
 }
