2bba6d4f0f5002d7572bce0fdb444ba06b9a3fb1
jaecoder222 <82952382+jaecoder222@users.noreply.github.com>
2022-11-27 21:36:53 +0900

    [level 0] Title: 가위 바위 보, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,13 @@
+const win = {
+    '2' : '0',
+    '0' : '5',
+    '5' : '2',
+}
+function solution(rsp) {
+    var answer = '';
+    for(let i = 0; i < rsp.length; i++){
+        const item = rsp[i]
+        answer += win[item]
+    }
+    return answer;
+}
