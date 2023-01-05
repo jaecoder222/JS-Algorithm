2752e61cda307b3fb2490e3de38d18a2087e273a
jaecoder222 <82952382+jaecoder222@users.noreply.github.com>
2023-01-05 14:03:22 +0900

    [level 0] Title: 소인수분해, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,18 @@
+function solution(n) {
+    let answer = [];
+
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        while (n % i === 0) {
+            answer.push(i);
+            n /= i;
+        }
+    }
+    
+    if(n >= 2){
+        answer.push(n);    
+    }
+    
+    let result = Array.from(new Set(answer));
+    
+    return result.sort((a,b) => a -b);
+}
