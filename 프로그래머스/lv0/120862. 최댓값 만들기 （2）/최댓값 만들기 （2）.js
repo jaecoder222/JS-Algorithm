e15e233e65bc24d98082da91b8caa76e432918d4
jaecoder222 <82952382+jaecoder222@users.noreply.github.com>
2023-01-25 01:06:29 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+ const result = [];
+    for(let i = 0; i < numbers.length; i++){
+        for(let j = 0; j < numbers.length; j++){
+            if(i !== j){
+                result.push(numbers[i] * numbers[j])
+            }
+        }
+    }
+    return Math.max(...result)
+}
