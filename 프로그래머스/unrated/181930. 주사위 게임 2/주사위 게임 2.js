b49e8f526573908445747a610a319da1a7b23bfb
jaecoder222 <82952382+jaecoder222@users.noreply.github.com>
2023-07-29 23:46:01 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,5 @@
+function solution(a, b, c) {
+    if(a === b && b === c && c === a) return (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+    if(a === b || b === c || c === a) return (a + b + c) * (a**2 + b**2 + c**2)
+    return a + b + c
+}
