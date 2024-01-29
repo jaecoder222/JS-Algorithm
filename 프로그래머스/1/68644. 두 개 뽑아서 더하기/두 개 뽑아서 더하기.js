6cc0cb594794cdd14abe51992960deaa25c96f0e
jaecoder222 <82952382+jaecoder222@users.noreply.github.com>
2024-01-29 09:48:22 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.83 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    let answer = []
+  for(let i = 0; i < numbers.length; i++){
+      for(let j =i+1; j < numbers.length; j++){
+          answer.push(numbers[i] + numbers[j])
+      }
+  }
+    
+  const newSet =  new Set(answer)
+  return [...newSet].sort((a,b)=> a-b)
+}
+
